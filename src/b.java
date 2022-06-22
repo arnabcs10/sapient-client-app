c6a69fc7d8071487f91bf40146da790c39861e35
Arnab Sengupta <arnab10panda@gmail.com>
2022-06-22 13:20:42 +0530

    merge conflict in b.java resolved

--- src/b.java
+++ src/b.java
@@ -1,9 +1,6 @@
 class MainB{
     public static void main(String args[]){
-<<<<<<< HEAD
         System.out.println("Hello I am user2");
-=======
         System.out.println("Hello I am user5");
->>>>>>> 8fdadec (user5 updated b.java)
     }
 }
