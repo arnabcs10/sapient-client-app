08fd333b15b91f6bee9b18e25cbf753203965ba9
Arnab Sengupta <arnab10panda@gmail.com>
2022-06-22 13:06:13 +0530

    user5 updated b.java Merge conflict resolved

--- src/b.java
+++ src/b.java
@@ -1,5 +1,9 @@
 class MainB{
     public static void main(String args[]){
+<<<<<<< HEAD
         System.out.println("Hello I am user2");
+=======
+        System.out.println("Hello I am user5");
+>>>>>>> 8fdadec (user5 updated b.java)
     }
 }
